b330fac48bbf6ad645ffa1a6a33120787dd3faea
Peregrine901 <cedrikaquino123@gmail.com>
2026-02-20 21:57:01 +0800

    Update grade_checker_program.py
    Checked

--- grade_checker_program.py
+++ grade_checker_program.py
@@ -1,27 +1,23 @@
 #LA 5
 
-while True:
-    try:
-        score = int(input("Enter your grade [0-100]: "))
 
-        grade = score
-        if score < 60:
-            grade = "F"
-        elif score >= 60 and score <= 69:
-            grade = "D"
+score = int(input("Enter your grade [0-100]: "))
 
-        elif score >= 70 and score <= 79:
-            grade = "C"
+grade = score
+if score < 60:
+    grade = "F"
+elif score >= 60 and score <= 69:
+    grade = "D"
 
-        elif score >= 80 and score <= 89:
-            grade = "B"
+elif score >= 70 and score <= 79:
+    grade = "C"
 
-        elif score >= 90 and score <= 100:
-            grade = "A"
-        elif score > 100:
-            grade = "A+"
-        
-        print(f'Score = {score} Grade = {grade}')
+elif score >= 80 and score <= 89:
+    grade = "B"
 
-    except ValueError:
-        print("Invalid Score")
+elif score >= 90 and score <= 100:
+    grade = "A"
+elif score > 100:
+    grade = "A+"
+
+print(f'Score = {score} Grade = {grade}')
